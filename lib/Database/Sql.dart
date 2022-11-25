748439e42de7c46c01b6eafa7cb99e48740eae26
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2022-11-25 20:02:26 +0530

    refactor register screen

--- lib/Database/Sql.dart
+++ lib/Database/Sql.dart
@@ -1,17 +1,61 @@
 import 'package:mysql1/mysql1.dart';
 
 Future<MySqlConnection> connect(
-    {String localhost,
+    {String host,
     int port,
     String userName,
     String password,
     String database}) async {
   var settings = new ConnectionSettings(
-      host: localhost,
-      port: port,
-      user: userName,
-      password: password,
-      db: database);
+      host: host, port: port, user: userName, password: password, db: database);
   var conn = await MySqlConnection.connect(settings);
   return conn;
+}
+
+//add user to table
+Future<void> addUser(MySqlConnection conn,
+    {String Name,
+    String Email,
+    String Mobile,
+    String Username,
+    Password}) async {
+  try {
+    var result = await conn.query(
+        'insert into Signin (Name,Email,Mobile,Username,Password) values (?, ?, ?, ?, ?)',
+        [Name, Email, Mobile, Username, Password]);
+  } on Exception catch (e) {
+    // TODO
+    print(e);
+  }
+}
+
+// check if given username and password is in table
+Future<bool> isUserInTable(MySqlConnection conn,
+    {String userName, String password}) async {
+  try {
+    var result = await conn.query(
+        'select * from Signin where userName = ? and password = ?',
+        [userName, password]);
+    if (result.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  } on Exception catch (e) {
+    // TODO
+    print(e);
+    return false;
+  }
+}
+
+//get car data from database
+Future<List> getCarData(MySqlConnection conn, String model) async {
+  var results = await conn.query('select * from car where model = ?', [model]);
+  return results.toList();
+}
+
+// book a car available in database
+Future<void> bookCar(MySqlConnection conn, String model) async {
+  var result =
+      await conn.query('update car set isBooked = 1 where model = ?', [model]);
 }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -14,96 +14,178 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "logo",
-              child: Container(
-                height: 200.0,
-                child: Image.asset('assets/images/acura_0.png'),
-              ),
-            ),
-            // SizedBox(
-            //   height: 5,
-            // ),
-            TextField(
-              onChanged: (value) {
-                //TODO Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Hero(
+                  tag: "logo",
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('assets/images/acura_0.png'),
+                  ),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.purpleAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.purpleAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //TODO Do something with the user input.
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your password',
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.purpleAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.purpleAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            Hero(
-              tag: "register",
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.deepPurpleAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () {
-                      //TODO Implement registration functionality.
-                      Navigator.pushNamed(context, Showroom.routeName);
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: Colors.black),
+                // SizedBox(
+                //   height: 5,
+                // ),
+                TextField(
+                  onChanged: (value) {
+                    //TODO Do something with the user input.
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter Username',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
                     ),
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //TODO Do something with the user input.
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter Name',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //TODO Do something with the user input.
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter Phone number',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //TODO Do something with the user input.
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    //TODO Do something with the user input.
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purpleAccent, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                Hero(
+                  tag: "register",
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Material(
+                      color: Colors.deepPurpleAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          //TODO Implement registration functionality.
+                          Navigator.pushNamed(context, Showroom.routeName);
+                        },
+                        minWidth: 200.0,
+                        height: 42.0,
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
